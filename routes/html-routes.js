0543a43ea4fffc9af9016e677f168cfdb092488d
Patty C <patrickcorcoran10@gmail.com>
2018-09-19 18:23:45 -0500

    html route to favs page added

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -28,4 +28,8 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
+  app.get("/members/favs", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/favs.html"));
+  });
+
 };
