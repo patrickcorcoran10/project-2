4a74d6f324fbf9d788aae48955f8637f57839712
Patty C <patrickcorcoran10@gmail.com>
2018-09-21 14:43:07 -0500

    commit that adds image and buttons to move from page to page. Those don't function.

--- public/js/members.js
+++ public/js/members.js
@@ -16,22 +16,23 @@ $.post("/api/yelpRequest", userInput, function (data) {
   // console.log(data);
   var businesses = data.jsonBody.businesses;
   for (var i = 0; i < 20; i++) {
+  // businesses[i];
   document.getElementById("restaurantName").innerHTML = businesses[i].name;
-  document.getElementById("restaurantImage").innerHTML = businesses[i].image_url;
+  document.getElementById("restaurantImage").innerHTML = ("<img src=" + businesses[i].image_url + " height=140 width=150 >");
   document.getElementById("restaurantType").innerHTML = businesses[i].categories[0].title;
   document.getElementById("restaurantLocation").innerHTML = businesses[i].location.display_address;
   document.getElementById("restaurantPhone").innerHTML = businesses[i].display_phone;
 
 
-  console.log(businesses[i].name);
-  console.log(businesses[i].image_url);
-  console.log(businesses[i].categories[0].title);
-  console.log(businesses[i].location.display_address);
-  console.log(businesses[i].display_phone);
+  // console.log(businesses[i].name);
+  // console.log(businesses[i].image_url);
+  // console.log(businesses[i].categories[0].title);
+  // console.log(businesses[i].location.display_address);
+  // console.log(businesses[i].display_phone);
 
   };
 })
-}
+};
 
 (function($) {
   "use strict";
